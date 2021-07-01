aff6706a31fb1e359c9057275ec99eb14e85072a
Fred Juhlin <fred@juhlin.me>
2021-07-01 16:57:22 +0200

    1.0.0

--- acs.js
+++ acs.js
@@ -6,8 +6,7 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
 		this.action = config.action;
 		this.camera = config.camera;
-		this.from = config.from;
-		this.to = config.to;
+		this.time = config.time;
 		this.duration = config.duration;
 		this.server = config.server;
         var node = this;
@@ -18,18 +17,9 @@ module.exports = function(RED) {
 			var password = server.credentials.password;
 			var action = msg.action || node.action;
 			var camera = msg.camera || node.camera;
-			var from = msg.from || node.from;
-			var to = msg.to || node.to;
-			var duration = msg.duration || node.duration;
-			if( typeof duration === "string" ) {
-				if( duration.length === 0 )
-					duration = 0;
-				duration = parseInt( duration );
-			}
-			if( duration < 0 )
-				duration = 0;
-			if( duration > 600 )
-				duration = 600;
+			var time = msg.time || node.time;
+			var duration = msg.duration || node.duration || 0;
+			duration = parseInt( duration );
 			switch( action ) {
 				case "Info":
 					var url = address + '/Acs/Api/SystemFacade/GetSystem';
@@ -98,20 +88,31 @@ module.exports = function(RED) {
 						node.send(msg);
 						return;
 					}
+					
 					var start = new Date();
+					if( typeof time === "string" ) {
+						if( time.length === 10 )
+							start = new Date(time + "T00:00:00");
+						if( time.length > 13 )
+							start = new Date(time);
+					}
+
+					if( typeof time === "number" && time > 1577836800000 )
+						start = new Date(time);
+					
+					if( typeof time === "object" )
+						start = new Date(time);
+					
 					start.setHours(0);
 					start.setMinutes(0);
 					start.setSeconds(0);
 					start.setMilliseconds(0);
-					var end = new Date();
-					if( typeof from === "number" && from > 1277924313000 )
-						start = new Date(from);
-					if( typeof from === "string" && from.length > 15 )
-						start = new Date(from);
-					if( typeof to === "number" && to > 1277924313000 )
-						end = new Date(to);
-					if( typeof to === "string" && to.length > 15 )
-						end = new Date(to);
+					if( duration < 1 )
+						duration = 1;
+					if( duration > 31 )
+						duration = 31;
+					
+					var end = new Date( start.getTime() + (duration*24*3600*1000));
 					
 					var request  = {
 						 "cameraIds": [{Id:camera}],
@@ -134,69 +135,69 @@ module.exports = function(RED) {
 								var recording = {
 							//            id: item.RecordingId,
 									track: item.QualityLevel,
-									start: start.getTime(),
-									stop: end.getTime(),
-									duration: 0  //parseInt( (to.getTime() - from.getTime()) / 1000 )
+									timestamp: 0,
+									date: "",
+									time: "",
+									duration: 0  //parseInt( (to.getTime() - time.getTime()) / 1000 )
 								}
-								
+								t1 = start;
 								if( item.hasOwnProperty("StartTime") ) {
 									item.StartTime[10] = 'T';
 									item.StartTime += 'Z';
-									recording.start = new Date(item.StartTime).getTime();
+									t1 = new Date(item.StartTime);
 								}
-									
+								var t2 = new Date();	
 								if( item.hasOwnProperty("EndTime") ) {
 									item.EndTime[10] = 'T';
 									item.EndTime += 'Z';
-									recording.stop = new Date(item.EndTime).getTime();
+									t2 = new Date(item.EndTime);
 								}
-								recording.duration = parseInt( ( recording.stop - recording.start) / 1000 );
+								recording.timestamp = t1.getTime();
+								recording.duration = parseInt( ( t2.getTime() - t1.getTime() ) / 1000 );
+								recording.date = t1.getFullYear()+"-"+('0'+(t1.getMonth()+1)).substr(-2,2)+"-"+('0'+t1.getDate()).substr(-2,2);
+								recording.time = ('0'+t1.getHours()).substr(-2,2) + ":" + ('0'+t1.getMinutes()).substr(-2,2) +":"+ ('0'+t1.getSeconds()).substr(-2,2);
 								list.push(recording);
 							});
 							msg.payload = list;
 							node.send(msg);
+							return;
 						} catch (error) {
 							msg.error = error;
 							msg.payload = null;
 							node.send(msg);
+							return;							
 						}
 					})();
 				break;
 				case "Get recording":
 					if( !camera || typeof camera !== "string" || camera.length < 15 ) {
-						msg.error = "Invalid camera ID";
+						msg.error = "Invalid ACS camera ID";
 						msg.payload = null;
 						node.send(msg);
 						return;
 					}
-					var start = null;
-					var end = null;
-
-					if( typeof from === "number" && from > 1277924313000 )
-						start = new Date(from);
-					if( typeof from === "string" && from.length > 15 )
-						start = new Date(from);
-					if( typeof to === "number" && to > 1277924313000 )
-						end = new Date(to);
-					if( typeof to === "string" && to.length > 15 )
-						end = new Date(to);
-
-					if( !start || !end ) {
-						msg.error = "Invalid from or to time";
-						msg.payload = null;
-						node.send(msg);
-					}
-
-					//Adjust for triggered recordings
-					start = new Date( start.getTime() + 1000);
-					end = new Date( end.getTime() - 1000);
 					
-					if( start.getTime() >= end.getTime() ) {
-						var t = start;
-						end = start;
-						start = t;
+					var start = new Date( new Date().getTime() - (60*1000) ); //Default to now - 1 minut if time is not set
+					if( typeof time === "string" ) {
+						if( time.length === 10 )
+							start = new Date(time + "T00:00:00");
+						if( time.length > 13 )
+							start = new Date(time);
 					}
 
+					if( typeof time === "number" && time > 1577836800000 )
+						start = new Date(time);
+					
+					if( typeof time === "object" )
+						start = new Date(time);
+
+					start = new Date( new Date(start) + 1000);
+					
+					if( duration < 10 )
+						duration = 60;
+
+					end = new Date( start.getTime() + (duration * 1000) - 2000);
+					
 					startTime = start.getUTCFullYear()+"-"+('0'+(start.getUTCMonth()+1)).substr(-2,2)+"-"+('0'+start.getUTCDate()).substr(-2,2) + "-";
 					startTime += ('0'+start.getUTCHours()).substr(-2,2) + ('0'+start.getUTCMinutes()).substr(-2,2) + ('0'+start.getUTCSeconds()).substr(-2,2)+"-0000000Z";
 					endTime = end.getUTCFullYear()+"-"+('0'+(end.getUTCMonth()+1)).substr(-2,2)+"-"+('0'+end.getUTCDate()).substr(-2,2) + "-";
@@ -213,6 +214,8 @@ module.exports = function(RED) {
 							const response = await got( url,{username: user,password:password,responseType: "buffer",https:{rejectUnauthorized: false}});
 							msg.error = false;
 							msg.payload = response.body;
+							msg.date = start.getFullYear()+"-"+('0'+(start.getMonth()+1)).substr(-2,2)+"-"+('0'+start.getDate()).substr(-2,2);
+							msg.time = ('0'+start.getHours()).substr(-2,2) + ":" +  ('0'+start.getMinutes()).substr(-2,2) + ":" +  ('0'+start.getSeconds()).substr(-2,2);
 							node.send(msg);
 						} catch (error) {
 							msg.error = error;
@@ -238,7 +241,6 @@ module.exports = function(RED) {
 						request.deactivateAfterSeconds = duration.toString();
 					}
 					url += encodeURI(JSON.stringify(request));
-					console.log(url);
 					(async () => {
 						try {
 							const response = await got( url,{username: user,password:password,responseType: "text",https:{rejectUnauthorized: false}});
@@ -262,7 +264,6 @@ module.exports = function(RED) {
 						return;
 					}
 					var url = address + "/Acs/Api/TriggerFacade/DeactivateTrigger?" + encodeURI(JSON.stringify({triggerName: camera}));
-					console.log(url);
 					(async () => {
 						try {
 							const response = await got( url,{username: user,password:password,responseType: "text",https:{rejectUnauthorized: false}});
@@ -292,8 +293,7 @@ module.exports = function(RED) {
 			server: {type:"ACS Server"},
 			camera: { type:"text" },
 			action: { type:"text" },
-			from: {type:"text"},
-			to: {type:"text"},
+			time: {type:"text"},
 			duration: {type:"text"}
 		}		
 	});
